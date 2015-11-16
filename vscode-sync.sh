18551a0c1063f794e7037686a028c8dbd9f3e64b
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2015-11-16 22:38:12 +0100

    Added vscode sync.

--- vscode-sync.sh
+++ vscode-sync.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 ultisnippets2vscode --in $(dirname $0)/bundle/ciplogic/UltiSnips/typescript.snippets \
-    --out ${HOME}/programs/vscode-0.9.1/resources/app/extensions/typescript/snippets/typescript.json
+    --out ${HOME}/programs/vscode-0.9.2/resources/app/extensions/typescript/snippets/typescript.json
+ultisnippets2vscode --in $(dirname $0)/bundle/ciplogic/UltiSnips/typescript.snippets \
+    --out ${HOME}/programs/vscode-0.9.2/resources/app/extensions/typescript/snippets/typescriptreact.json
 
